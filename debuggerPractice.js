170d518a3514053ad80bc78e95cc55435ac33e1f
megumi29 <megumisan90@gmail.com>
2020-07-23 05:16:49 -0400

    updated answers and branch name for LoopsPartOne

--- debuggerPractice.js
+++ debuggerPractice.js
@@ -1,9 +1,9 @@
 function addToSentence(sentence, word) {
-    if (sentence.length > 0) {
-        sentence += " ";
-    }
-    sentence += word;
-    return sentence;
+  if (sentence.length > 0) {
+    sentence += " ";
+  }
+  sentence += word;
+  return sentence;
 }
 
 let words = ["The", "red", "fox", "jumped", "over", "the", "dog"];
@@ -11,10 +11,11 @@ let sentence = "";
 let index = 0;
 
 while (index < words.length) {
-    let word = words[index];
-    sentence = addToSentence(sentence, word);
-    index++
+  let word = words[index];
+  sentence = addToSentence(sentence, word);
+  index++;
 }
 sentence += ".";
 
 console.log(sentence);
+//
